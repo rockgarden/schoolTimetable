c5753b210bb758f387bbd1385117d6a743533c73
rockgarden <rockwk@msn.com>
2016-02-24 14:37:16 +0800

    add project files

---
+++ mylesson/mylessonTests/mylessonTests.swift
@@ -0,0 +1,36 @@
+//
+//  mylessonTests.swift
+//  mylessonTests
+//
+//  Created by wk on 16/2/19.
+//  Copyright © 2016年 wk. All rights reserved.
+//
+
+import XCTest
+@testable import mylesson
+
+class mylessonTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
